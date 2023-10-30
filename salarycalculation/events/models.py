011e15a74fa8d766b377ec3be54169548ae6a421
Notebook <pavel1182@yandex.ru>
2023-10-30 13:43:26 +0300

    Create model Event

---
+++ salarycalculation/events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    comment = models.CharField(max_length=200)
+    date_of_creation = models.DateTimeField('Date of creation')
+    date_of_the_event = models.DateTimeField('Date of the event')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None)

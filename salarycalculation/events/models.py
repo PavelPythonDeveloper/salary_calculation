d09cacdf06234bae180380f62267ad3298e07e1e
Notebook <pavel1182@yandex.ru>
2023-11-05 22:57:14 +0300

    add __str__ method to model

--- salarycalculation/events/models.py
+++ salarycalculation/events/models.py
@@ -10,8 +10,11 @@ class Event(models.Model):
     comment = models.CharField(max_length=200)
     date_of_creation = models.DateTimeField(default=timezone.now)
     date_of_the_event = models.DateTimeField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events', default=None)
     price = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['date_of_the_event']
+
+    def __str__(self):
+        return self.title

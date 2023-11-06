35bfb24b611697795e5cf31fcab300cf2bc76800
Notebook <pavel1182@yandex.ru>
2023-11-06 14:50:15 +0300

    Add empty_event_list_msg variable
    it displays if events variable is empty

--- salarycalculation/events/views.py
+++ salarycalculation/events/views.py
@@ -5,11 +5,12 @@ from .models import Event
 
 def events_list(request):
     events = Event.objects.all()
-    print(events)
-    return render(request, 'events/event/list.html', {'events': events})
+    empty_event_list_msg = 'You have no event. Add one?'
+    return render(request, 'events/event/list.html',
+                  {'events': events,
+                   'empty_event_list_msg': empty_event_list_msg})
 
 
 def events_detail(request, id):
     event = get_object_or_404(Event, id=id)
     return render(request, 'events/event/detail.html', {'event': event})
-

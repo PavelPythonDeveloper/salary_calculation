0ae778f67b5a7ed0a2abb5c3caf03cb344658600
Notebook <pavel1182@yandex.ru>
2023-11-06 19:09:46 +0300

    Add create_event function and test_event_detail_id_available test

--- salarycalculation/events/tests.py
+++ salarycalculation/events/tests.py
@@ -1,5 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Event
+from django.utils import timezone
+from datetime import timedelta
+from django.contrib.auth.models import User
+
+
+def create_event(title, comment, date_of_the_event, price):
+    creator = User()
+    creator.save()
+    event = Event.objects.create(title=title,
+                                 comment=comment,
+                                 date_of_the_event=date_of_the_event,
+                                 price=price,
+                                 creator=creator)
+    return event
 
 
 class EventEventsListViewTest(TestCase):
@@ -11,3 +26,14 @@ class EventEventsListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'You have no event. Add one?')
         self.assertQuerySetEqual(response.context['events'], [])
+
+
+class EventEventsDetailViewTest(TestCase):
+    def test_event_detail_is_available(self):
+        create_event(title='Testing',
+                     comment='We test our code sometimes',
+                     date_of_the_event=timezone.now() + timedelta(days=3),
+                     price=1000
+                     )
+        response = self.client.get(reverse('events:events_detail', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, 200)

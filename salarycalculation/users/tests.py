2959b02f44f291a39d049351113cdb99234cbe11
Notebook <pavel1182@yandex.ru>
2023-11-08 23:17:53 +0300

    Add test_is_new_user_registered test for register view

--- salarycalculation/users/tests.py
+++ salarycalculation/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class UsersRegisterViewTest(TestCase):
+    def test_is_new_user_registered(self):
+        response = self.client.post(reverse('users:register'), data={
+            'username': 'Tester',
+            'email': 'tester@testing.te',
+            'password1': 'testerpassword',
+            'password2': 'testerpassword'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(User.objects.filter(username='Tester').exists())

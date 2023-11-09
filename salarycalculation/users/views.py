e25386adb3691c7dcbcbb21efb3c6307124770d4
Notebook <pavel1182@yandex.ru>
2023-11-09 14:02:09 +0300

    Add login_user view

--- salarycalculation/users/views.py
+++ salarycalculation/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm, UserLoginForm
 
 
 def register(request):
@@ -16,3 +17,17 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+def login_user(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('events:events_list', user_id=user.id)
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})

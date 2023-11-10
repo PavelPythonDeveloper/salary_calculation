9cf07c261059edd2ff3bc323f28b5d57a495f321
Notebook <pavel1182@yandex.ru>
2023-11-10 21:05:16 +0300

    Change path to register_done template

--- salarycalculation/users/views.py
+++ salarycalculation/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
 from .forms import UserRegisterForm, UserLoginForm
 
@@ -12,22 +13,22 @@ def register(request):
         if form.is_valid():
             new_user = form.save()
             context = {'new_user': new_user}
-            return render(request, 'users/register_done.html', context=context)
+            return render(request, 'registration/register_done.html', context=context)
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('events:events_list', user_id=user.id)
-    else:
-        form = UserLoginForm()
-    return render(request, 'users/login.html', {'form': form})
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('events:events_list', user_id=user.id)
+#     else:
+#         form = UserLoginForm()
+#     return render(request, 'users/login.html', {'form': form})

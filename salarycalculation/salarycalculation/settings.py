56d4095204042d86ff99483f4963dfed71826da3
Notebook <pavel1182@yandex.ru>
2023-11-10 20:59:29 +0300

    Add LOGIN_URL and LOGOUT_URL

--- salarycalculation/salarycalculation/settings.py
+++ salarycalculation/salarycalculation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events.apps.EventsConfig',
-    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN_REDIRECT_URL = 'list'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'

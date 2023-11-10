88ae9e38447db2b91760c00c6accf24994261357
Notebook <pavel1182@yandex.ru>
2023-11-10 21:06:23 +0300

    Delete own login_user function

--- salarycalculation/users/views.py
+++ salarycalculation/users/views.py
@@ -19,16 +19,3 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect('events:events_list', user_id=user.id)
-#     else:
-#         form = UserLoginForm()
-#     return render(request, 'users/login.html', {'form': form})

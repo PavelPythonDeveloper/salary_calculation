8b8afd8ca8396eb75400ffdc956c4ce94ca6393d
Notebook <pavel1182@yandex.ru>
2023-11-10 20:58:42 +0300

    Add login_required decorators for events_list and events_detail view functions

--- salarycalculation/events/views.py
+++ salarycalculation/events/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Event
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def events_list(request, user_id):
     events = Event.objects.all()
     empty_event_list_msg = 'You have no event. Add one?'
@@ -13,6 +15,7 @@ def events_list(request, user_id):
                   )
 
 
+@login_required()
 def events_detail(request, id):
     event = get_object_or_404(Event, id=id)
     return render(request, 'events/event/detail.html', {'event': event})

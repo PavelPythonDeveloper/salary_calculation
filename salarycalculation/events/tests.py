627278a8c8ac888e35cf776b51b57de5a67f3655
Notebook <pavel1182@yandex.ru>
2023-11-13 22:26:50 +0300

    Add test user login Refactor comment in test_no_events

--- salarycalculation/events/tests.py
+++ salarycalculation/events/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Event
 from django.utils import timezone
@@ -6,24 +6,24 @@ from datetime import timedelta
 from django.contrib.auth.models import User
 
 
-def create_event(title, comment, date_of_the_event, price):
-    creator = User()
-    creator.save()
-    # print(creator)
+def create_event(title, comment, date_of_the_event, price, creator):
     event = Event.objects.create(title=title,
                                  comment=comment,
                                  date_of_the_event=date_of_the_event,
                                  price=price,
                                  creator=creator)
+
     return event
 
 
 class EventEventsListViewTest(TestCase):
     def test_no_events(self):
         """
-        If no events exist, an appropriate message is displayed.
+        If user has no events, an appropriate message is displayed.
         """
-        response = self.client.get(reverse('events:events_list', kwargs={'user_id': 1}))
+        User.objects.create_user(username='tester', password='12345')
+        self.client.login(username='tester', password='12345')
+        response = self.client.get(reverse('events:events_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'You have no event. Add one?')
         self.assertQuerySetEqual(response.context['events'], [])
@@ -31,13 +31,18 @@ class EventEventsListViewTest(TestCase):
 
 class EventEventsDetailViewTest(TestCase):
     def test_event_detail_is_available(self):
-        create_event(title='Testing',
-                     comment='We test our code sometimes',
-                     date_of_the_event=timezone.now() + timedelta(days=3),
-                     price=1000
-                     )
-        response = self.client.get(reverse('events:events_detail', kwargs={'id': 1}))
+        user = User.objects.create_user(username='tester', password='12345')
+        logged_in = self.client.login(username='tester', password='12345')
+        event = create_event(title='Testing',
+                             comment='We test our code sometimes',
+                             date_of_the_event=timezone.now() + timedelta(days=3),
+                             price=1000,
+                             creator=user)
+
+        response = self.client.get(reverse('events:events_detail', kwargs={'id': event.id}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Testing')
         self.assertContains(response, 'We test our code sometimes')
         self.assertContains(response, '1000')
+
+        

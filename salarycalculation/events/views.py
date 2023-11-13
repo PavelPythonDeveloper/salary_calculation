f1b283f998866234520e80e03106ee2cfe5de6fd
Notebook <pavel1182@yandex.ru>
2023-11-13 22:34:51 +0300

    Remove user_id parameter in events_list Add event filtering by creator in events_list Add check event creator
    User id is taken from the request object

--- salarycalculation/events/views.py
+++ salarycalculation/events/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.http import HttpResponse
 from .models import Event
 from django.contrib.auth.decorators import login_required
 
 
-@login_required()
-def events_list(request, user_id):
-    events = Event.objects.all()
+@login_required
+def events_list(request):
+    events = Event.objects.filter(creator=request.user)
     empty_event_list_msg = 'You have no event. Add one?'
     return render(request, 'events/event/list.html',
                   {'events': events,
                    'empty_event_list_msg': empty_event_list_msg,
-                   'user_id': user_id},
+                   'user_id': request.user.id},
                   )
 
 
-@login_required()
+@login_required
 def events_detail(request, id):
     event = get_object_or_404(Event, id=id)
-    return render(request, 'events/event/detail.html', {'event': event})
+    if event.creator == request.user:
+        return render(request, 'events/event/detail.html', {'event': event})
+    return HttpResponse("You can't view other people's events")

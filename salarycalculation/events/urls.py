7de3b1ef9e920005bc355f25cb0d8f14b93c5c60
Notebook <pavel1182@yandex.ru>
2023-11-15 14:06:53 +0300

    Add urlpattern for calculate view

--- salarycalculation/events/urls.py
+++ salarycalculation/events/urls.py
@@ -7,4 +7,5 @@ app_name = 'events'
 urlpatterns = [
     path('list/', views.events_list, name="events_list"),
     path('detail/<int:id>/', views.events_detail, name="events_detail"),
+    path('calculate/', views.calculate, name='calculate'),
 ]

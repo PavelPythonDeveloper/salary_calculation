6a943d674c4a3aa454b761e8758dff97a5cbb569
Notebook <pavel1182@yandex.ru>
2023-12-15 12:51:02 +0300

    Add CreateNewEventForm

--- salarycalculation/events/forms.py
+++ salarycalculation/events/forms.py
@@ -4,3 +4,10 @@ from django import forms
 class CalculateSumForm(forms.Form):
     start = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"])
     end = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"])
+
+
+class CreateNewEventForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    date_of_the_event = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"])
+    comment = forms.CharField(max_length=100)
+    price = forms.IntegerField()

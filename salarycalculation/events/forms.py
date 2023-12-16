fec1b7606ef4307785897fcfa058b23d234738e4
Notebook <pavel1182@yandex.ru>
2023-12-16 21:12:43 +0300

    Add min value in price field

--- salarycalculation/events/forms.py
+++ salarycalculation/events/forms.py
@@ -10,4 +10,4 @@ class CreateNewEventForm(forms.Form):
     title = forms.CharField(max_length=50)
     date_of_the_event = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"])
     comment = forms.CharField(max_length=100)
-    price = forms.IntegerField()
+    price = forms.IntegerField(min_value=0)

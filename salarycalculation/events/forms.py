ac1a42a4786247e50948501abb546e6f4284f1cc
Notebook <pavel1182@yandex.ru>
2023-12-20 20:15:10 +0300

    Add another input format to DateTimeField

--- salarycalculation/events/forms.py
+++ salarycalculation/events/forms.py
@@ -8,6 +8,6 @@ class CalculateSumForm(forms.Form):
 
 class CreateNewEventForm(forms.Form):
     title = forms.CharField(max_length=50)
-    date_of_the_event = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M"])
+    date_of_the_event = forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M", "%d.%m.%Y %H:%M:%S"])
     comment = forms.CharField(max_length=100)
     price = forms.IntegerField(min_value=0)

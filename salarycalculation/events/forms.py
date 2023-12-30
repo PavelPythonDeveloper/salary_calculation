c530086b65cd1b40d93f51878bb4935d54bb2148
Notebook <pavel1182@yandex.ru>
2023-12-30 23:19:41 +0300

    Remove widgets from fields

--- salarycalculation/events/forms.py
+++ salarycalculation/events/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core import validators
 
 
 class DateInput(forms.DateInput):
@@ -10,15 +12,15 @@ class TimeInput(forms.TimeInput):
 
 
 class CalculateSumForm(forms.Form):
-    start_date = forms.DateTimeField(input_formats=["%d.%m.%Y"], widget=DateInput)
-    start_time = forms.TimeField(input_formats=["%H:%M"], widget=TimeInput)
-    end_date = forms.DateTimeField(input_formats=["%d.%m.%Y"], widget=DateInput)
-    end_time = forms.TimeField(input_formats=["%H:%M"], widget=TimeInput)
+    start_date = forms.DateField(input_formats=["%d.%m.%Y"])
+    start_time = forms.TimeField(input_formats=["%H:%M"])
+    end_date = forms.DateField(input_formats=["%d.%m.%Y"])
+    end_time = forms.TimeField(input_formats=["%H:%M"])
 
 
 class CreateNewEventForm(forms.Form):
     title = forms.CharField(max_length=50)
-    date_of_the_event = forms.DateTimeField(input_formats=["%d.%m.%Y"], widget=DateInput)
-    time_of_the_event = forms.TimeField(input_formats=["%H:%M"], widget=TimeInput)
-    comment = forms.CharField(max_length=100)
+    date_of_the_event = forms.DateField(input_formats=["%d.%m.%Y"])
+    time_of_the_event = forms.TimeField(input_formats=["%H:%M:%S", '%H:%M'])
+    comment = forms.CharField(max_length=100, required=False)
     price = forms.IntegerField(min_value=0)

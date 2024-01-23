283ee215822e623ae84e078393d38fe1de5cb761
Notebook <pavel1182@yandex.ru>
2024-01-23 18:54:13 +0300

    Remove unused path

--- salarycalculation/events/urls.py
+++ salarycalculation/events/urls.py
@@ -6,7 +6,6 @@ app_name = 'events'
 
 urlpatterns = [
     path('list/', views.events_list, name="events_list"),
-    path('detail/<int:id>/', views.events_detail, name="events_detail"),
     path('calculate/', views.calculate, name='calculate'),
     path('create/', views.create_new_event, name='create'),
     path('delete/<int:id>/', views.delete_event, name='delete'),

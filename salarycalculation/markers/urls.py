5f261940819dd922eec9692cb194b2f79a60226f
Notebook <pavel1182@yandex.ru>
2024-02-26 13:37:04 +0300

    Fix bug with creating markers

--- salarycalculation/markers/urls.py
+++ salarycalculation/markers/urls.py
@@ -8,6 +8,6 @@ app_name = "markers"
 urlpatterns = [
     # path('create/', views.create_marker, name='create_marker'),
     path('detail/<int:marker_id>/', views.markers_detail, name='markers_detail'),
-    path('add_marker_to_event/<int:event_id>', views.add_marker_to_event, name='add_marker_to_event'),
+    path('add_marker_to_event/<str:event_id>', views.add_marker_to_event, name='add_marker_to_event'),
     path('list/', views.markers_list, name='markers_list'),
 ]
--- salarycalculation/markers/views.py
+++ salarycalculation/markers/views.py
@@ -18,32 +18,35 @@ from events.models import Event
 
 @login_required
 def markers_list(request):
+    context = {}
     if request.method == 'POST':
-        form = CreateNewMarkerForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            m = Marker(creator=request.user, name=name)
-            m.save()
-            return redirect('markers:markers_list')
+        if request.GET.get('for_event'):
+            form = CreateNewMarkerForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                m = Marker(creator=request.user, name=name)
+                m.save()
+                return redirect('markers:markers_list')
     form = CreateNewMarkerForm()
-    if request.GET.get('for-event'):
-        event = Event.objects.get(id=request.GET.get('for-event'))
-
+    if request.GET.get('for_event'):
+        event = Event.objects.get(id=request.GET.get('for_event'))
+        context.update(event=event)
     markers = request.user.markers.all()
-    return render(request, 'markers/list.html', {'markers': markers, 'form': form, 'event': event})
+    context.update(markers=markers, form=form)
+    return render(request, 'markers/list.html', context)
 
 
 @login_required
 def add_marker_to_event(request, event_id):
-
     if request.method == 'POST':
         choices = request.POST.getlist('choice')
-        event = Event.objects.get(id=event_id)
-        for marker in request.user.markers.all():
-            if str(marker.id) not in choices:
-                event.markers.remove(marker)
-            else:
-                event.markers.add(marker)
+        if event_id != 'None':
+            event = Event.objects.get(id=int(event_id))
+            for marker in request.user.markers.all():
+                if str(marker.id) not in choices:
+                    event.markers.remove(marker)
+                else:
+                    event.markers.add(marker)
 
         page = request.GET.get('event_page', 1)
         return HttpResponseRedirect(reverse('events:events_list') + f'?page={page}')

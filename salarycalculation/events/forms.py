dc59f556f3d29c24b6236eac01f7e570c8ffa963
Notebook <pavel1182@yandex.ru>
2024-06-17 13:29:39 +0300

    Add datetime widgets in calculate form

--- salarycalculation/events/forms.py
+++ salarycalculation/events/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from django.contrib.admin.widgets import AdminDateWidget
+from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
+from widgets import CustomDatePickerWidget
 
 
 # class CustomAdminDateWidget(forms.DateInput):
@@ -18,17 +19,18 @@ from django.contrib.admin.widgets import AdminDateWidget
 
 
 class CalculateSumForm(forms.Form):
-    start_date = forms.DateField(label=_('start date'), input_formats=["%d.%m.%Y"])
-    start_time = forms.TimeField(label=_('start time'), input_formats=["%H:%M"])
-    end_date = forms.DateField(label=_('end date'), input_formats=["%d.%m.%Y"])
-    end_time = forms.TimeField(label=_('end time'), input_formats=["%H:%M"])
+    start_date = forms.DateField(widget=AdminDateWidget, label=_('start date'), input_formats=["%d.%m.%Y"])
+    start_time = forms.TimeField(widget=AdminTimeWidget, label=_('start time'), input_formats=["%H:%M"])
+    end_date = forms.DateField(widget=AdminDateWidget, label=_('end date'), input_formats=["%d.%m.%Y"])
+    end_time = forms.TimeField(widget=AdminTimeWidget, label=_('end time'), input_formats=["%H:%M"])
 
 
 class CreateNewEventForm(forms.Form):
     title = forms.CharField(label=_('title'), max_length=50)
     date_of_the_event = forms.DateField(widget=AdminDateWidget, label=_('date of the event'),
                                         input_formats=["%d.%m.%Y", "%Y-%m-%d"])
-    time_of_the_event = forms.TimeField(label=_('time of the event'), input_formats=["%H:%M:%S", '%H:%M'])
+    time_of_the_event = forms.TimeField(widget=AdminTimeWidget, label=_('time of the event'),
+                                        input_formats=["%H:%M:%S", '%H:%M'])
     comment = forms.CharField(label=_('comment'), max_length=100, required=False)
     paid = forms.BooleanField(label=_('paid'), required=False)
     price = forms.IntegerField(label=_('price'), min_value=0)

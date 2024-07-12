873d205b8602d71e8d5058344b3ce09e03dd01c9
Notebook <pavel1182@yandex.ru>
2024-07-12 20:47:19 +0300

    Фdd an init method to handle the user instance

--- salarycalculation/presets/forms.py
+++ salarycalculation/presets/forms.py
@@ -4,8 +4,15 @@ from markers.models import Marker
 
 
 class CreateNewPresetForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(CreateNewPresetForm, self).__init__(*args, **kwargs)
+        self.fields['markers'].queryset = Marker.objects.filter(creator=self.user)
+
     title = forms.CharField(max_length=200)
     comment = forms.CharField(max_length=200)
     time_of_the_event = forms.TimeField()
     price = forms.IntegerField()
-    markers = forms.ModelMultipleChoiceField(widget=SelectMultiple, queryset=Marker.objects.none())
+    markers = forms.ModelMultipleChoiceField(widget=SelectMultiple, queryset=None)
+
